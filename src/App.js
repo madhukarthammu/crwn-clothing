b47c246ce77ffb520d8e0e1df4c35c78b36f0c22
Madhukar Bavanasi <madhukarthammu1@gmail.com>
2020-09-09 23:52:29 +0530

    node_modules be gone!

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Homepage from './pages/homepage.component'
+
+function App() {
+  return (
+    <div className="App">
+        <Homepage />
+    </div>
+  );
+}
+
+export default App;
