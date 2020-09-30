81a2bd0337f20f6051e837316f93d6d98f057e95
Madhukar Bavanasi <madhukarthammu1@gmail.com>
2020-09-30 16:09:46 +0530

    added redux persist library to keep the cartitems ideal  whenever user refreshes the page

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import  store  from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-  <Router>
-    <App />
-  </Router>
+    <Router>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,10 +3,21 @@ import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
 import directoryReducer from './directory/directory.reducer'
 import shopReducer from './shop/shop.reducer'
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+  };
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
     directory: directoryReducer,
     shop: shopReducer
 })
+
+export default persistReducer(persistConfig, rootReducer);
+
